a94c26466853acd00b07c6127d6aa586309a940e
Eric <eric.m.lombardo@gmail.com>
2020-12-07 11:11:43 -0500

    created music and dr. model that utalized 'has many through' relationships

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,15 @@
+class Appointment
+  attr_accessor :date, :patient, :doctor 
 
+  @@all = []  # collects all appt. instances
+  def initialize(date, patient, doctor)
+    @date = date        # starts with date, patient, and doctor
+    @patient = patient  # allows access to each branch
+    @doctor = doctor    # this is the single source of truth
+    @@all << self
+  end
+
+  def self.all  # reader method for @@all
+    @@all
+  end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,26 @@
+class Patient
+  attr_accessor :name
 
+  @@all = []  # keeps track of all patients
+  def initialize(name)
+    @name = name  # starts with a name
+    @@all << self # pushes instances to @@all array
+  end
+  
+  def self.all  # getter method for @@all
+    @@all
+  end
+
+  def new_appointment(date, doctor) # creates new appt., links date, patient, dr.
+    new_appointment = Appointment.new(date, self, doctor)
+  end 
+
+  def appointments # returns all appt.'s for linked to patient
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors # returns all dr.'s linked to this patient
+    appointments.map {|appointment| appointment.doctor}
+  end
+
+end
